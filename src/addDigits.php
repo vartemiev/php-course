0fe60a73cabe9b73744f2c656510ecbfe2e025ff
Vsevolod Artemev <vartemev@plesk.com>
2022-06-22 07:54:59 +0300

    Fixes after review

--- src/addDigits.php
+++ src/addDigits.php
@@ -2,11 +2,12 @@
 function addDigits(int $num)
 {
     $sum = 0;
+    $stringNum = (string)$num;
 
-    $stringNum = (string) $num;
-    for ($i = 0; $i < strlen($stringNum); $i++) {
-        $sum += (int) $stringNum[$i];
+    $stringLen = strlen($stringNum);
+    for ($i = 0; $i < $stringLen; $i++) {
+        $sum += (int)$stringNum[$i];
     }
 
-    return strlen((string) $sum) > 1 ? addDigits($sum) : $sum;
+    return strlen((string)$sum) > 1 ? addDigits($sum) : $sum;
 }
